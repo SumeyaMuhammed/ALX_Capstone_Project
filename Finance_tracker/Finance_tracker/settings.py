d8cd50525ffad0439d01f8ba96ef55b6dac5d3d7
Sumeya Muhammed <sumenaweya@gmail.com>
2025-12-23 05:13:00 +0300

    reuse postgresql for both

--- Finance_tracker/Finance_tracker/settings.py
+++ Finance_tracker/Finance_tracker/settings.py
@@ -90,24 +90,17 @@ WSGI_APPLICATION = 'Finance_tracker.wsgi.application'
 #     )
 # }
 
-if os.getenv('RAILWAY_ENVIRONMENT'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
+
 
 
 
